09aa01d44d9cdad27db0ca7d4ff3136337b1c751
chidea <sbw228@gmail.com>
2022-06-15 01:57:26 +0900

    replace LocalStorage with IndexedDB, index page renamed to "

--- bu.ps1
+++ bu.ps1
@@ -1,8 +1,8 @@
 . ./env
 trunk build --release
 if ($?) {
-  rm $trg_path/index.html
   rm $trg_path/*.js
   rm $trg_path/*.wasm
   cp dist/* $trg_path/
+  cp dist/index.html $trg_path/404.html
 }
--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,22 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 use log::debug;
 
 use gloo_timers::future::TimeoutFuture;
-use js_sys::Date;
-use sycamore::{builder::prelude::*, futures::spawn_local_scoped, prelude::*};
+use js_sys::{Date, };
+use sycamore::{/* builder::prelude::*,  */futures::spawn_local_scoped, suspense::Suspense, prelude::*};
 use sycamore_router::{HistoryIntegration, Router};
-use wasm_bindgen::JsCast;
-use web_sys::{Event, /* HtmlInputElement,  */ HtmlTextAreaElement, InputEvent};
+use wasm_bindgen::{JsCast, closure::Closure, prelude::*};
+use wasm_bindgen_futures::JsFuture;
+
+use web_sys::{Event, /* HtmlInputElement,  */ HtmlTextAreaElement, InputEvent, IdbDatabase, IdbOpenDbRequest, IdbObjectStore};
 
 use creole_nom::prelude::*;
 
 mod route;
 use route::AppRoutes;
 
-// todo : light/dark theme
+use urlencoding::decode;
+
+// TODO : light/dark theme
 #[derive(Debug, Default, Clone)]
 pub struct AppState {
     pub theme: RcSignal<Theme>,
@@ -30,14 +34,6 @@ impl Default for Theme {
     fn default() -> Self {
         Theme::Light
     }
-}
-
-fn open_local_storage() -> web_sys::Storage {
-    let window = web_sys::window().expect("no global `window` exists");
-    window
-        .local_storage()
-        .unwrap()
-        .expect("user has not enabled localStorage")
 }
 
 fn measure(perf: &web_sys::Performance, name: &str, s: &str, e: &str) {
@@ -85,8 +81,13 @@ fn CreoleItem<'a, G: Html>(cx: Scope<'a>, i: ICreole<'a>) -> View<G> {
             if href.starts_with("http://") || href.starts_with("https://") {
                 view! { cx, a(href=href, target="__blank") { (format!("{t}")) } }
             } else {
-                let on_click = move |_| sycamore_router::navigate(&format!("/w/{}", href));
-                view! { cx, a(href="#", on:click=on_click) { (format!("{t}")) } }
+                let path = format!("/w/{}", href);
+                let p = path.clone();
+                let on_click = move |e: Event| {
+                  e.prevent_default();
+                  sycamore_router::navigate(&p);
+                };
+                view! { cx, a(href=path, on:click=on_click) { (format!("{t}")) } }
             }
         }
         ICreole::Line(l) => creole_as_node(cx, "p", l),
@@ -131,7 +132,7 @@ fn CreoleItem<'a, G: Html>(cx: Scope<'a>, i: ICreole<'a>) -> View<G> {
 
 #[derive(Prop)]
 struct CreolePreviewProps<'a> {
-    value: &'a ReadSignal<Box<str>>,
+    value: &'a ReadSignal<String>,
     show_title: bool,
     // parsed: &'a ReadSignal<Vec<ICreole<'a>>>,
     // value: Vec<ICreole<'a>>,
@@ -171,12 +172,16 @@ fn CreolePreview<'a, G: Html>(cx: Scope<'a>, props: CreolePreviewProps<'a>) -> V
 }
 #[derive(Prop)]
 struct CreoleEditorProps<'a> {
-    value: &'a Signal<Box<str>>,
-    // parsed: &'a Signal<Vec<ICreole<'a>>>,
-    path: String,
+    default: String,
+    value: &'a Signal<String>,
 }
 const HELP : &str = "= Help
-== [[https://webassembly.org|WASM]] [[http://www.wikicreole.org|Creole]] //Live// Editor ([[https://github.com/chidea/wasm-creole-live-editor|github]])
+== important note
+Everything you 'edit' in this app stays in your browser(into IndexedDB).
+
+Using any other device, browser, domain, even protocol or clearing your browser cache will erase every note of yourself.
+
+Browsers may limit or ask you for storage expansion when total saved notes are becoming larger than its maximum.
 ----
 == text styles
 //italic// and **bold**.
@@ -210,11 +215,11 @@ const HELP : &str = "= Help
 * a
 ----
 == external links
+[[https://webassembly.org|WASM]]
+
+[[http://www.wikicreole.org|WikiCreole]]
+
 [[https://www.w3schools.com/]]
-
-[[https://webassembly.org]]
-
-[[http://www.wikicreole.org]]
 
 == script links
 [[javascript:alert('hi')|alert me \"hi\"]]
@@ -262,31 +267,13 @@ Force\\\\linebreak
 ";
 #[component]
 fn CreoleEditor<'a, G: Html>(cx: Scope<'a>, props: CreoleEditorProps<'a>) -> View<G> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let local_storage = window
-        .local_storage()
-        .unwrap()
-        .expect("user has not enabled localStorage");
     let node_ref = create_node_ref(cx);
 
     let last_update = create_signal(cx, 0.);
     let updated = create_signal(cx, false);
-
-    let default_value = if !props.path.is_empty() {
-        if let Ok(Some(v)) = local_storage.get_item(&props.path) {
-            v
-        } else if props.path == "help" {
-            // no user note for home page
-            String::from(HELP)
-        } else {
-            String::new()
-        }
-    } else {
-        String::new()
-    }
-    .into_boxed_str();
-
+    let default_value = props.default;
     {
+        let window = web_sys::window().expect("no global `window` exists");
         spawn_local_scoped(cx, async move {
             loop {
                 TimeoutFuture::new(500).await;
@@ -304,15 +291,7 @@ fn CreoleEditor<'a, G: Html>(cx: Scope<'a>, props: CreoleEditorProps<'a>) -> Vie
                     perf.clear_measures();
                     perf.mark("s1").unwrap_or(());
                 }
-                let s = e.value();
-                // debug!("input : {}", s);
-                if s.is_empty() {
-                    local_storage.remove_item(&props.path).unwrap_or(());
-                } else if !props.path.is_empty() && props.path != "help" {
-                    local_storage.set_item(&props.path, &s).unwrap_or(());
-                }
-
-                props.value.set(s.into_boxed_str());
+                props.value.set(e.value().into());
 
                 if let Some(perf) = &perf {
                     perf.mark("e1").unwrap_or(());
@@ -347,37 +326,105 @@ struct CreoleProps {
 }
 
 #[component]
-fn Creole<G: Html>(cx: Scope, props: CreoleProps) -> View<G> {
+async fn Creole<G: Html>(cx: Scope<'_>, props: CreoleProps) -> View<G> {
+    let store = open_db_store(cx, props.editable);
+    let path = props.path.clone();
+    debug!("getting : {}", path);
+
+    let value =
+      if path == "help" {
+          String::from(HELP)
+      } else if let Ok(r) = store.get(&path.into()) {
+        JsFuture::from(js_sys::Promise::new(&mut |resolve : js_sys::Function, _reject: js_sys::Function|{
+          let cb = Closure::once(move |e: &Event| -> Result<(), JsValue> {
+            if let Some(t) = e.target() {
+              resolve.call1(&JsValue::NULL,
+                &t.unchecked_into::<web_sys::IdbRequest>().result().expect("")
+              ).expect("");
+            }
+            Ok(())
+          });
+          r.set_onsuccess(Some(create_ref(cx, cb).as_ref().unchecked_ref()));
+        })).await.expect("promise").as_string().unwrap_or(String::new())
+      } else {
+        String::new()
+      };
+
+    let value_signal : &Signal<String> = create_signal(cx, value.clone());
+
     if props.editable {
-        let value = create_signal(cx, String::new().into_boxed_str());
+        let path : JsValue = props.path.clone().into();
+        create_effect(cx, move || {
+          let store = open_db_store(cx, props.editable);
+          let value = &*value_signal.get();
+          let path = path.as_string().unwrap();
+          debug!("saving to : {}, value : {}", path, value);
+          if path != "help" {
+            if value.is_empty() {
+              if let Ok(_r) = store.delete(&path.into()) {
+              }
+            } else if let Ok(_r) = store.put_with_key(&value.into(), &path.into()) {
+            }
+          }
+        });
         view! { cx,
           div(class="wrapper") {
             CreoleEditor {
-              value: value,
-              path: props.path,
+              value: value_signal,
+              default: value,
             }
-            CreolePreview{ value :value, show_title: true }
+            CreolePreview{ value :value_signal, show_title: true }
           }
         }
     } else {
-        let local_storage = open_local_storage();
-        let value = create_signal(
-            cx,
-            if let Ok(Some(v)) = local_storage.get_item(&props.path) {
-                v
-            } else {
-                String::new()
-            }
-            .into_boxed_str(),
-        );
         view! { cx,
-          CreolePreview{ value : value, show_title: false }
+          CreolePreview{ value : value_signal, show_title: false }
         }
     }
 }
 
+const DB_NAME: &str = "wiki";
+const STORE_NAME: &str = "wiki";
+
+async fn init_db(cx: Scope<'_>) -> IdbDatabase {
+    JsFuture::from(js_sys::Promise::new(&mut |resolve : js_sys::Function, _reject: js_sys::Function|{
+        let window = web_sys::window().expect("no global `window` exists");
+        let req = window
+            .indexed_db().unwrap().expect("user has not enabled IndexedDB")
+            .open(DB_NAME).expect("wiki DB is not available");
+        let a = Closure::once(move |evt: &web_sys::IdbVersionChangeEvent| -> Result<(), JsValue> {
+          if let Some(t) = evt.target() {
+            let wiki_db = t.unchecked_into::<IdbOpenDbRequest>().result().expect("")
+              .unchecked_into::<IdbDatabase>();
+            let wiki_store = wiki_db.create_object_store(STORE_NAME)?;
+            wiki_store.put_with_key(&HELP.into(), &"".into())?;
+          }
+          Ok(())
+        });
+        req.set_onupgradeneeded(Some(a.as_ref().unchecked_ref()));
+        a.forget();
+        let b = Closure::once(move |e: &Event| -> Result<(), JsValue> {
+          if let Some(t) = e.target() {
+            resolve.call1(&JsValue::NULL, &t.unchecked_into::<IdbOpenDbRequest>().result().expect("")).expect("");
+          }
+          Ok(())
+        });
+        req.set_onsuccess(Some(create_ref(cx, b).as_ref().unchecked_ref()));
+    })).await.expect("promise").unchecked_into()
+}
+fn open_db_store(cx: Scope, write: bool) -> IdbObjectStore {
+    let db = use_context::<RcSignal<IdbDatabase>>(cx);
+    let transaction = db.get().transaction_with_str_and_mode(STORE_NAME, 
+      if write { web_sys::IdbTransactionMode::Readwrite }
+      else { web_sys::IdbTransactionMode::Readonly }).expect("could not open transaction");
+    transaction.object_store(STORE_NAME).expect("could not open store")
+}
 #[component]
-fn App<G: Html>(cx: Scope) -> View<G> {
+async fn App<G: Html>(cx: Scope<'_>) -> View<G> {
+    let db = create_rc_signal(init_db(cx).await);
+    debug!("db opened : {:?}, name : {}", db, db.get().name());
+    provide_context(cx, db);
+
     let wiki_path_node_ref = create_node_ref(cx);
     let wiki_path = create_signal(cx, String::new());
 
@@ -385,18 +432,16 @@ fn App<G: Html>(cx: Scope) -> View<G> {
         wiki_path.set(s.clone());
         s
     };
-    let on_edit = |_| sycamore_router::navigate(&format!("/e/{}", *wiki_path.get()));
-    let on_view = |_| sycamore_router::navigate(&format!("/w/{}", *wiki_path.get()));
-    let on_del = |_| sycamore_router::navigate(&format!("/d/{}", *wiki_path.get()));
 
     view! { cx,
       nav {
-        a(href="#", on:click=|_|sycamore_router::navigate("/")) { button{ ("Home(=Edit 'home')") } }
-        a(href="#", on:click=|_|sycamore_router::navigate("/help")) { button{ ("Help") } }
+        button(on:click=|_| sycamore_router::navigate("/w/")) { ("Home view") }
+        button(on:click=|_| sycamore_router::navigate("/e/")) { ("Home edit") }
+        button(on:click=|_|sycamore_router::navigate("/help")) { ("Help") }
         input(type="text", bind:value=wiki_path, ref=wiki_path_node_ref) { }
-        button(on:click=on_edit){ ("Edit") }
-        button(on:click=on_view){ ("View") }
-        button(on:click=on_del){ ("Delete") }
+        button(on:click=|_| sycamore_router::navigate(&format!("/e/{}", *wiki_path.get()))){ ("Edit") }
+        button(on:click=|_| sycamore_router::navigate(&format!("/w/{}", *wiki_path.get()))){ ("View") }
+        button(on:click=|_| sycamore_router::navigate(&format!("/d/{}", *wiki_path.get()))){ ("Delete") }
       }
       Router {
         integration: HistoryIntegration::new(),
@@ -404,52 +449,55 @@ fn App<G: Html>(cx: Scope) -> View<G> {
           use AppRoutes::*;
           view! { cx,
             div(class="app") {
-              (match route.get().as_ref() {
-                Index => {
-                  view! { cx,
-                    Creole{ editable: true, path: set_wiki_path(String::from("home")) }
-                  }
-                },
-                Help => {
-                  view! { cx,
-                    Creole { editable: true, path: String::from("help") }
-                  }
-                },
-                Wiki{path} => {
-                  view! { cx,
-                    Creole { editable: false, path: set_wiki_path(path.join("/")) }
-                  }
-                },
-                WikiEdit{path} => {
-                  view! { cx,
-                    Creole { editable: true, path: set_wiki_path(path.join("/")) }
-                  }
-                },
-                WikiDelete{path} => {
-                  let p = path.join("/");
-                  wiki_path.set(p.clone());
-                  let pp = p.clone();
-                  let on_del_yes = move |_|{
-                    let window = web_sys::window().expect("no global `window` exists");
-                    let local_storage = window
-                        .local_storage()
-                        .unwrap()
-                        .expect("user has not enabled localStorage");
-                    local_storage.remove_item(&pp).unwrap_or(());
-                    sycamore_router::navigate("/");
-                  };
-                  view! { cx,
-                    p {
-                      h2 { "delete?" }
-                      button(on:click=on_del_yes) { ("Yes")}
+              Suspense {
+                fallback: view!{ cx, "Reading from DB..."},
+                (match route.get().as_ref() {
+                  Index => {
+                    view! { cx,
+                      Creole{ editable: false, path: set_wiki_path(String::new()) }
                     }
-                    Creole { editable: false, path: p }
-                  }
-                },
-                NotFound => view! { cx,
-                  "404 Not Found"
-                },
-              })
+                  },
+                  Help => {
+                    view! { cx,
+                      Creole { editable: true, path: String::from("help") }
+                    }
+                  },
+                  Wiki{path} => {
+                    let path = path.into_iter().map(|s: &String| decode(s).expect("UTF8").into_owned()).collect::<Vec<String>>();
+                      // decode(&path.join("/")).expect("UTF8").into_owned();
+                    view! { cx,
+                      Creole { editable: false, path: set_wiki_path(path.join("/")) }
+                    }
+                  },
+                  WikiEdit{path} => {
+                    let path = path.into_iter().map(|s: &String| decode(s).expect("UTF8").into_owned()).collect::<Vec<String>>();
+                    view! { cx,
+                      Creole { editable: true, path: set_wiki_path(path.join("/")) }
+                    }
+                  },
+                  WikiDelete{path} => {
+                    let path = path.into_iter().map(|s: &String| decode(s).expect("UTF8").into_owned()).collect::<Vec<String>>();
+                    let p = path.join("/");
+                    wiki_path.set(p.clone());
+                    let pp : std::cell::RefCell<JsValue> = std::cell::RefCell::new(p.clone().into());
+                    let on_del_yes = move |_| {
+                      let store = open_db_store(cx, true);
+                      if let Ok(_) = store.delete(&pp.take()){}
+                      sycamore_router::navigate("/");
+                    };
+                    view! { cx,
+                      p {
+                        h2 { "delete?" }
+                        button(on:click=on_del_yes) { ("Yes")}
+                      }
+                      Creole { editable: false, path: p }
+                    }
+                  },
+                  NotFound => view! { cx,
+                    "404 Not Found"
+                  },
+                })
+              }
             }
           }
         }
@@ -461,5 +509,11 @@ fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).unwrap();
 
-    sycamore::render(|cx| component(|| App(cx, ())));
+    // sycamore::render(|cx| component(|| App(cx, ())));
+    sycamore::render(|cx| view! { cx,
+      Suspense {
+        fallback: view!{ cx, "Opening DB..."},
+        App {}
+      }
+    });
 }
